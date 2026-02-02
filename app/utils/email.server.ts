a8c9f1ee56f2572c819fcf01fcd5d838b6ecb098
alexo382 <alexandrumanuelolteanu@gmail.com>
2026-02-02 14:55:38 +0200

    more email updates

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -31,7 +31,7 @@ export async function sendEmail({
 	| { html: string; text: string; react?: never }
 	| { react: ReactElement; html?: never; text?: never }
 )) {
-	const from = 'onboarding@resend.dev'
+	const from = 'hello@useqm.app'
 
 	const email = {
 		from,
